1efc65630b0f25a3ad9ffae6abf77dba1ee5e739
jonahhontos <jonahhontos@gmail.com>
2026-07-05 23:08:46 -0700

    update character speed/acc to an exported var

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,13 +1,4 @@
 extends Character
 class_name Player
 
-var player_speed: float = 225.0
-var player_acceleration: float = 15.0
-
 var tool_1_action: Global.Tools = Global.Tools.SWORD
-
-
-func _ready() -> void:
-	super._ready()
-	speed = player_speed
-	acceleration = player_acceleration
